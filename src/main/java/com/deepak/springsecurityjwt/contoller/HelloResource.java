6922b73d8076412cb6db0e4f464c3c52e582f2a6
Deepak Gyawali <deepakgyawali@Deepaks-MacBook-Pro.local>
2020-08-23 14:59:06 -0500

    My First Commit

---
+++ src/main/java/com/deepak/springsecurityjwt/contoller/HelloResource.java
@@ -0,0 +1,12 @@
+package com.deepak.springsecurityjwt.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+        @RequestMapping("/hello")
+        public String hello(){
+           return "Hello World";
+        }
+}
